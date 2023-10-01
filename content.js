366bc3b8da3524cf5e8c67043c6c182e0e4ec9fb
fabian <godsonhm3@gmail.com>
2023-10-01 01:00:46 +0100

    add video recorder controls popup

--- content.js
+++ content.js
@@ -1,65 +1,335 @@
-console.log("Hi, I have been injected whoopie!!!")
+let recordingTimerInterval;
+let recordingTime = 0;
+let isRecording = false;
+var recorder = null;
 
-var recorder = null
-function onAccessApproved(stream){
-    recorder = new MediaRecorder(stream);
+// Function to create and display recording controls
+function createRecordingControls() {
+  const recordingControls = document.createElement("div");
+  recordingControls.id = "recording-controls";
+  recordingControls.innerHTML = `<aside
+      class="recorder_container"
+      style="
+        display: flex;
+        padding: 0.5rem;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+        border-radius: 12.5rem;
+        background: rgba(98, 98, 98, 0.17);
+        width: fit-content;
+        position: fixed;
+        bottom: 60px;
+        left: 60px;
+        zIndex: 9999;
+      "
+    >
+      <div
+        class="recorder"
+        style="
+          display: flex;
+          padding: 0.75rem 2.5rem;
+          justify-content: center;
+          align-items: center;
+          gap: 1.5rem;
+          border-radius: 12.5rem;
+          background: #141414;
+        "
+      >
+        <div
+          class="timer_box"
+          style="display: flex; align-items: center; gap: 0.81rem"
+        >
+          <strong
+            id="timer"
+            style="
+              color: #fff;
+              font-family: Inter, sans-serif;
+              font-size: 1.25rem;
+              font-weight: 500;
+            "
+            >00:00:00</strong
+          >
+          <div
+            class="sign"
+            style="
+              width: 0.625rem;
+              height: 0.625rem;
+              border-radius: 1.0625rem;
+              background: #c00404;
+              border-radius: 50%;
+            "
+          ></div>
+        </div>
 
-    recorder.start();
+        <div
+          class="divider"
+          style="width: 0.0625rem; height: 3rem; background: #e8e8e8"
+        ></div>
 
-    recorder.onstop = function(){
-        stream.getTracks().forEach(function(track){
-            if(track.readyState === "live"){
-                track.stop()
-            }
-        })
-    }
+        <button class="control pause" style="background: transparent; border: none">
+          <div
+            id="pause"
+            style="
+              display: flex;
+              cursor: pointer;
+              gap: 0.5rem;
+              width: 1.5rem;
+              height: 1.5rem;
+              padding: 0.625rem;
+              place-content: center;
+              border-radius: 50%;
+              border: 1px solid #fff;
+              background: #fff;
+              margin-bottom: 0.5rem;
+            "
+          >
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094899/Icons/Line_23_wvwxjf.svg"
+              alt="line"
+            />
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094899/Icons/Line_23_wvwxjf.svg"
+              alt="line"
+            />
+          </div>
+          <strong
+            style="
+              color: #fff;
+              font-family: Work Sans, sans-serif;
+              font-size: 0.75rem;
+              font-weight: 500;
+            "
+            >Pause</strong
+          >
+        </button>
 
-    recorder.ondataavailable = function(event){
-        let recordedBlob  = event.data;
-        let url = URL.createObjectURL(recordedBlob);
+        <button class="control stop" style="background: transparent; border: none">
+          <div
+            style="
+              display: grid;
+              width: 1.5rem;
+              height: 1.5rem;
+              cursor: pointer;
+              padding: 0.625rem;
+              place-content: center;
+              border-radius: 50%;
+              border: 1px solid #fff;
+              background: #fff;
+              margin-bottom: 0.5rem;
+            "
+          >
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094899/Icons/stop_a43eip.svg"
+              alt="stop"
+            />
+          </div>
+          <strong
+            style="
+              color: #fff;
+              font-family: Work Sans, sans-serif;
+              font-size: 0.75rem;
+              font-weight: 500;
+            "
+            >Stop</strong
+          >
+        </button>
 
-        let a = document.createElement("a");
+        <button class="control" style="background: transparent; border: none">
+          <div
+            style="
+              display: grid;
+              width: 1.5rem;
+              height: 1.5rem;
+              cursor: pointer;
+              padding: 0.625rem;
+              place-content: center;
+              border-radius: 50%;
+              border: 1px solid #fff;
+              background: #fff;
+              margin-bottom: 0.5rem;
+            "
+          >
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094899/Icons/video-camera_uc4wcw.svg"
+              alt="camera"
+            />
+          </div>
+          <strong
+            style="
+              color: #fff;
+              font-family: Work Sans, sans-serif;
+              font-size: 0.75rem;
+              font-weight: 500;
+            "
+            >Camera</strong
+          >
+        </button>
 
-        a.style.display = "none";
-        a.href = url;
-        a.download = "screen-recording.webm"
+        <button class="control" style="background: transparent; border: none">
+          <div
+            style="
+              display: grid;
+              width: 1.5rem;
+              height: 1.5rem;
+              cursor: pointer;
+              padding: 0.625rem;
+              place-content: center;
+              border-radius: 50%;
+              border: 1px solid #fff;
+              background: #fff;
+              margin-bottom: 0.5rem;
+            "
+          >
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094899/Icons/microphone_cthllc.svg"
+              alt="microphone"
+            />
+          </div>
+          <strong
+            style="
+              color: #fff;
+              font-family: Work Sans, sans-serif;
+              font-size: 0.75rem;
+              font-weight: 500;
+            "
+            >Mic</strong
+          >
+        </button>
 
-        document.body.appendChild(a);
-        a.click();
+        <button
+          class="control delete"
+          style="background: transparent; border: none"
+        >
+          <div
+            style="
+              display: grid;
+              width: 1.5rem;
+              height: 1.5rem;
+              padding: 0.625rem;
+              cursor: pointer;
+              place-content: center;
+              border-radius: 50%;
+              border: 1px solid #fff;
+              background: #fff;
+              margin-bottom: 1.25rem;
+            "
+          >
+            <img
+              src="https://res.cloudinary.com/dikeogwu1/image/upload/v1696094902/Icons/delete_icon_hsirly.svg"
+              alt="delete"
+            />
+          </div>
+        </button>
+      </div>
+    </aside>`;
 
-        document.body.removeChild(a);
+  document.body.appendChild(recordingControls);
+  const pauseBtn = recordingControls.querySelector(".pause");
+  const stopBtn = recordingControls.querySelector(".stop");
+  const deletBtn = recordingControls.querySelector(".delete");
 
-        URL.revokeObjectURL(url);
-    }
+  pauseBtn.addEventListener("click", pauseRecording);
+  stopBtn.addEventListener("click", stopRecording);
+  deletBtn.addEventListener("click", stopRecording);
 }
 
+// Function to update the recording timer
+function updateRecordingTimer() {
+  recordingTime++;
+  const hours = Math.floor(recordingTime / (60 * 60));
+  const minutes = Math.floor(recordingTime / 60);
+  const seconds = recordingTime % 60;
+  const timerElement = document.getElementById("timer");
+  timerElement.textContent = `${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
 
-chrome.runtime.onMessage.addListener( (message, sender, sendResponse)=>{
+// Function to start recording
+function startRecording() {
+  createRecordingControls();
+  recordingTimerInterval = setInterval(updateRecordingTimer, 1000);
+}
 
-    if(message.action === "request_recording"){
-        console.log("requesting recording")
+// Function to pause recording
+function pauseRecording() {
+  recorder.pause();
+  clearInterval(recordingTimerInterval);
+  // Remove the recording controls from the screen
+  const recordingControls = document.querySelector("#recording-controls");
+  recordingControls.remove();
+}
+// Function to stop recording
+function stopRecording() {
+  recorder.stop();
+  recordingTime = 0;
+  clearInterval(recordingTimerInterval);
+  // Remove the recording controls from the screen
+  const recordingControls = document.querySelector("#recording-controls");
+  recordingControls.remove();
+}
 
-        sendResponse(`processed: ${message.action}`);
+// ***** MAIN *****
 
-        navigator.mediaDevices.getDisplayMedia({
-            audio:true,
-            video: {
-                width:9999999999,
-                height: 9999999999
-            }
-        }).then((stream)=>{
-            onAccessApproved(stream)
-        })  
-    }
+function onAccessApproved(stream) {
+  recorder = new MediaRecorder(stream);
+  recorder.start();
+  startRecording();
 
-    if(message.action === "stopvideo"){
-        console.log("stopping video");
-        sendResponse(`processed: ${message.action}`);
-        if(!recorder) return console.log("no recorder")
+  recorder.onpause = function () {
+    stream.getTracks().forEach(function (track) {
+      if (track.readyState === "live") {
+        track.stop();
+      }
+    });
+  };
+  recorder.onstop = function () {
+    stream.getTracks().forEach(function (track) {
+      if (track.readyState === "live") {
+        track.stop();
+      }
+    });
+  };
 
-        recorder.stop();
+  recorder.ondataavailable = function (event) {
+    let recordedBlob = event.data;
+    let url = URL.createObjectURL(recordedBlob);
 
+    let a = document.createElement("a");
 
-    }
+    a.style.display = "none";
+    a.href = url;
+    a.download = "screen-recording.webm";
 
-})
+    document.body.appendChild(a);
+    // a.click();
+
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  };
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "request_recording") {
+    sendResponse(`processed: ${message.action}`);
+
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: true,
+      })
+      .then((stream) => {
+        onAccessApproved(stream);
+      });
+  }
+
+  if (message.action === "stopvideo") {
+    console.log("stopping video");
+    sendResponse(`processed: ${message.action}`);
+    if (!recorder) return console.log("no recorder");
+
+    stopRecording();
+  }
+});
--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,54 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    // GET THE SELECTORS OF THE BUTTONS
-    const startVideoButton = document.querySelector("button#start_video")
-    const stopVideoButton = document.querySelector("button#stop_video")
+document.addEventListener("DOMContentLoaded", () => {
+  // GET THE SELECTORS OF THE BUTTONS
+  const startVideoButton = document.querySelector(
+    "button#start-recording-button"
+  );
+  const stopVideoButton = document.querySelector("button#camera-switch");
+  const btns = document.querySelectorAll(".switch-btn");
+  const controlsBox = document.querySelector(".recorder_box");
 
-    // adding event listeners
+  // adding event listeners
 
-    startVideoButton.addEventListener("click", ()=>{
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {action: "request_recording"},  function(response){
-                if(!chrome.runtime.lastError){
-                    console.log(response)
-                } else{
-                    console.log(chrome.runtime.lastError, 'error line 14')
-                }
-            })
-        } )
-    })
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      if (!btn.classList.contains("slide")) {
+        btn.classList.add("slide");
+      } else {
+        btn.classList.remove("slide");
+      }
+    });
+  });
 
+  startVideoButton.addEventListener("click", () => {
+    // sendMessage
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "request_recording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 14");
+          }
+        }
+      );
+    });
+  });
 
-    stopVideoButton.addEventListener("click", ()=>{
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {action: "stopvideo"},  function(response){
-                if(!chrome.runtime.lastError){
-                    console.log(response)
-                } else{
-                    console.log(chrome.runtime.lastError, 'error line 27')
-                }
-            })
-        } )
-    })
-})
+  stopVideoButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "stopvideo" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 27");
+          }
+        }
+      );
+    });
+  });
+});
